3928de57dc2d6854e8c453d4d9a58feba4d87787
PTOM76 <ptms76@outlook.jp>
2026-05-08 19:15:46 +0900

    update mod version to 0.0.5 and refactor energy storage initialization in EnergyCableBlockEntity

--- src/main/java/net/pitan76/nexton/dynamics/block/entity/AbstractEnergyBlockEntity.java
+++ src/main/java/net/pitan76/nexton/dynamics/block/entity/AbstractEnergyBlockEntity.java
@@ -4,58 +4,10 @@ import net.pitan76.mcpitanlib.api.event.block.TileCreateEvent;
 import net.pitan76.mcpitanlib.midohra.block.entity.BlockEntityTypeWrapper;
 import net.pitan76.nexton.core.api.block.entity.MachineBlockEntity;
 import net.pitan76.nexton.core.api.energy.EnergyStorageProvider;
-import net.pitan76.nexton.core.api.energy.IEnergyStorage;
-import net.pitan76.nexton.core.api.energy.SimpleEnergyStorage;
 
 public abstract class AbstractEnergyBlockEntity extends MachineBlockEntity implements EnergyStorageProvider {
-    public IEnergyStorage energyStorage = null;
 
     public AbstractEnergyBlockEntity(BlockEntityTypeWrapper type, TileCreateEvent e) {
         super(type.get(), e);
     }
-
-    @Override
-    public IEnergyStorage getEnergyStorage() {
-        if (energyStorage == null) {
-            energyStorage = new SimpleEnergyStorage.Builder().capacity(getUsableCapacity()).maxInput(getMaxInputEnergy()).maxOutput(getMaxOutputEnergy()).canInsert(canInsertEnergy()).canExtract(canExtractEnergy()).build();
-        }
-
-        return energyStorage;
-    }
-
-    public boolean addEnergy(long energy) {
-        if (canAddEnergy(energy)) {
-            this.setEnergyStored(getEnergyStored() + energy);
-            return true;
-        }
-        return false;
-    }
-
-    public boolean removeEnergy(long energy) {
-        return addEnergy(-energy);
-    }
-
-    public boolean canAddEnergy(long energy) {
-        return this.getCapacityEnergy() >= this.getEnergyStored() + energy && this.getEnergyStored() + energy >= 0;
-    }
-
-//    public long insertEnergy(long amount) {
-//        long usableCapacity = this.getUsableCapacity();
-//        if (amount > usableCapacity) {
-//            this.energy += usableCapacity;
-//            return usableCapacity;
-//        }
-//        this.energy += amount;
-//        return amount;
-//    }
-//
-//    public long extractEnergy(long amount) {
-//        if (amount > this.energy) {
-//            long energy = this.energy;
-//            this.energy = 0;
-//            return energy;
-//        }
-//        this.energy -= amount;
-//        return amount;
-//    }
 }
--- src/main/java/net/pitan76/nexton/dynamics/block/entity/EnergyCableBlockEntity.java
+++ src/main/java/net/pitan76/nexton/dynamics/block/entity/EnergyCableBlockEntity.java
@@ -1,26 +1,33 @@
 package net.pitan76.nexton.dynamics.block.entity;
 
 import net.pitan76.mcpitanlib.api.event.block.TileCreateEvent;
-import net.pitan76.mcpitanlib.api.event.nbt.ReadNbtArgs;
-import net.pitan76.mcpitanlib.api.event.nbt.WriteNbtArgs;
 import net.pitan76.mcpitanlib.api.event.tile.TileTickEvent;
 import net.pitan76.mcpitanlib.api.tile.ExtendBlockEntityTicker;
 import net.pitan76.mcpitanlib.core.datafixer.Pair;
 import net.pitan76.mcpitanlib.midohra.block.entity.BlockEntityTypeWrapper;
+import net.pitan76.nexton.core.api.energy.IEnergyStorage;
+import net.pitan76.nexton.core.api.energy.SimpleEnergyStorage;
 import net.pitan76.nexton.dynamics.CableNetworkManager;
 import net.pitan76.nexton.dynamics.Config;
 import java.util.*;
 
 public class EnergyCableBlockEntity extends AbstractEnergyBlockEntity implements ExtendBlockEntityTicker<EnergyCableBlockEntity> {
 
-    public EnergyCableBlockEntity(BlockEntityTypeWrapper type, TileCreateEvent e) {
-        super(type, e);
-        this.speed = Config.getEnergyCableTransferRate();
-    }
+    public final IEnergyStorage energyStorage;
 
     public EnergyCableBlockEntity(BlockEntityTypeWrapper type, TileCreateEvent e, int speed) {
         super(type, e);
         this.speed = speed;
+        energyStorage = new SimpleEnergyStorage.Builder().capacity(speed * 4L).maxInput(speed).maxOutput(speed).canInsert(true).canExtract(true).build();
+    }
+
+    public EnergyCableBlockEntity(BlockEntityTypeWrapper type, TileCreateEvent e) {
+        this(type, e, Config.getEnergyCableTransferRate());
+    }
+
+    @Override
+    public IEnergyStorage getEnergyStorage() {
+        return energyStorage;
     }
 
     public EnergyCableBlockEntity(TileCreateEvent e) {
@@ -32,21 +39,6 @@ public class EnergyCableBlockEntity extends AbstractEnergyBlockEntity implements
     }
 
     public final int speed;
-
-    @Override
-    public long getCapacityEnergy() {
-        return (long) speed * 4;
-    }
-
-    @Override
-    public long getMaxOutputEnergy() {
-        return speed;
-    }
-
-    @Override
-    public long getMaxInputEnergy() {
-        return speed;
-    }
 
     public UUID networkId = UUID.randomUUID();
 
@@ -63,15 +55,5 @@ public class EnergyCableBlockEntity extends AbstractEnergyBlockEntity implements
         if (!isMaster) return;
 
         network.tick();
-    }
-
-    @Override
-    public void writeNbt(WriteNbtArgs args) {
-        super.writeNbt(args);
-    }
-
-    @Override
-    public void readNbt(ReadNbtArgs args) {
-        super.readNbt(args);
     }
 }
